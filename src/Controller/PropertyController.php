aa917e08364369e44bc7dfa209ec4aa05ed7e74f
citizenz7 <citizenz7@protonmail.com>
2020-10-11 17:29:35 +0200

    first commit

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PropertyController extends AbstractController
+{
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+
+    /**
+     * @Route("/biens", name="property_index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('property/index.html.twig', [
+            'current_menu' => 'properties'
+        ]);
+    }
+
+    /**
+     * @Route("/biens/{slug}-{id}", name="property_show", requirements={"slug": "[a-z0-9\-]*"})
+     * @return Response
+     */
+    public function show(Property $property, string $slug): Response
+    {
+        // toujours rediriger vers le bon slug du moment que l'ID est ok
+        // même si on se trompe dans le slug...
+        if ($property->getSlug() !== $slug) {
+            return $this->redirectToRoute('property_show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ], 301);
+        }
+
+        return $this->render('property/show.html.twig', [
+            'property' => $property,
+            'current_menu' => 'properties'
+        ]);
+    }
+}
